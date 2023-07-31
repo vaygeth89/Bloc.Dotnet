83c580f236fc04829335d816e9405b4c3b6e0493
Abdulmohsen Alkhamees <vaygeth@gmail.com>
2023-07-31 08:06:54 +0300

    added dispose

--- Bloc.Models/BlocBase.cs
+++ Bloc.Models/BlocBase.cs
@@ -2,11 +2,10 @@ namespace Bloc.Models;
 
 public abstract class BlocBase<TState> where TState : BlocState
 {
-    public  TState State { get; internal set; }
+    public TState State { get; internal set; }
     protected abstract void Emit(TState newState);
 
     public abstract event Action<TState>? OnStateChanged;
 
     public abstract void Dispose();
-
 }
